79db2f8b744251a94360c3ba0ceb1081ad9c15c5
t1prog <tiport.prog@gmail.com>
2025-09-29 08:51:10 +0700

    вот так лучше

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -8,24 +8,6 @@ const Counter = () => {
         <Background>
             <CounterContainer>
                 <Toggle />
-                <Card style={{ width: "18rem" }}>
-                    <Card.Header>
-                        <Card.Title>
-                            <Row>
-                                <Col>
-                                    <Stack className="">
-                                        <div>just react.</div>
-                                        <div>just counter.</div>
-                                    </Stack>
-                                </Col>
-                                <Col className="d-flex align-items-center ">
-                                    <div>ts version.</div>
-                                </Col>
-                            </Row>
-                        </Card.Title>
-                    </Card.Header>
-                    <Card.Body></Card.Body>
-                </Card>
             </CounterContainer>
         </Background>
     );
